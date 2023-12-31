fd4efb4410f5a9064162e7e525ab8c425a0ef9d0
Devansh Kumar <kdevharsh2001@gmail.com>
2023-12-31 07:11:35 -0500

    Initial UI

--- vtc/src/App.js
+++ vtc/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+  const [mode, setMode] = useState('conservative');
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleModeChange = (selectedMode) => {
+    setMode(selectedMode);
+  };
+
+  const handleParaphrase = () => {
+    // Here you would call the paraphrasing API
+    // For the example, we're just mocking the output
+    setOutputText(`Paraphrased (mode: ${mode}): ${inputText}`);
+  };
+
+  const isModeActive = (selectedMode) => {
+    return mode === selectedMode ? 'active' : '';
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Code Summarization</h1>
       </header>
+      <main>
+       
+        <div className="mode-buttons">
+          <button className={isModeActive('conservative')} onClick={() => handleModeChange('conservative')}>Most conservative</button>
+          <button className={isModeActive('standard')} onClick={() => handleModeChange('standard')}>Standard Mode</button>
+          <button className={isModeActive('fluent')} onClick={() => handleModeChange('fluent')}>Fluent Mode</button>
+          <button className={isModeActive('creative')} onClick={() => handleModeChange('creative')}>Creative Mode</button>
+        </div>
+        <div className='Text'>
+        <div className = "input-area">
+        <textarea
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Enter the text you want to paraphrase"
+        />
+        </div>
+        <div className = "output-text">
+        <textarea
+          value={outputText}
+          readOnly
+          placeholder="You will get the resulting text here after the paraphrasing tool has finished rephrasing."
+        />
+        </div>
+        </div>
+        <button onClick={handleParaphrase}>Paraphrase</button>
+      </main>
     </div>
   );
 }
